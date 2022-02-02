0372b5e4674b46ca3bcba171f47e73b7e8035d92
Peter Phan <peterphan0422@gmail.com>
2022-02-02 17:50:33 -0600

    fixed: url from https to http

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,7 @@ const config = {};
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
 	config.API_URL = "http://localhost:5000";
 } else {
-	config.API_URL = "https://ec2-3-86-87-201.compute-1.amazonaws.com:5000";
+	config.API_URL = "http://ec2-3-86-87-201.compute-1.amazonaws.com:5000";
 }
 
 export default config;
--- src/organisms/ModalCart.js
+++ src/organisms/ModalCart.js
@@ -1,6 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const ModalCart = ({ isCartOpen, setIsCartOpen }) => {
+	const cart = useSelector((state) => state.cartReducer);
+
+	const renderedCartItems = cart.map((cartItem) => {
+		return <div key={cartItem.itemID}></div>;
+	});
+
 	return (
 		<div
 			className={`fixed top-0 left-0 w-full h-full z-30 transform ${
@@ -9,12 +16,25 @@ const ModalCart = ({ isCartOpen, setIsCartOpen }) => {
 			onClick={() => setIsCartOpen(false)}
 		>
 			<div
-				className={`fixed top-0 right-0 shadow-md w-96 h-full min-h-screen bg-blue-600 transition duration-300 ease-linear ${
+				className={`flex flex-col fixed top-0 right-0 shadow-md p-5 w-96 h-full min-h-screen bg-gray-100 transition duration-300 ease-linear ${
 					isCartOpen ? "" : "transform translate-x-full"
 				}`}
 				onClick={(e) => e.stopPropagation()}
 			>
-				hello world
+				<button
+					className="flex self-end text-lg text-gray-500 mb-5"
+					onClick={() => setIsCartOpen(false)}
+				>
+					<i className="fas fa-times"></i>
+				</button>
+				{cart[0] ? (
+					<h1 className="text-2xl font-headers font-bold">Your order</h1>
+				) : (
+					<div className="flex flex-col justify-center items-center font-body">
+						<p className="text-lg font-bold">Your cart is empty</p>
+						<p className="text-sm">Add items to get started</p>
+					</div>
+				)}
 			</div>
 		</div>
 	);
