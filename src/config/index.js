54a930cb0882b694690d79920cb4f493867b08ed
Peter Phan <peterphan0422@gmail.com>
2022-02-02 20:52:20 -0600

    fixed: config api url changed

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,9 @@
 const config = {};
 
-if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "development") {
 	config.API_URL = "http://localhost:5000";
 } else {
-	config.API_URL = "http://ec2-3-86-87-201.compute-1.amazonaws.com";
+	config.API_URL = "https://dashchef.herokuapp.com";
 }
 
 export default config;
