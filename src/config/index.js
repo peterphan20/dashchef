08b2083a510cac6b7a61d449e05c13829af41532
Peter Phan <peterphan0422@gmail.com>
2022-02-25 19:46:18 -0600

    feat: reset for vite migration

--- src/config/index.js
+++
@@ -1,9 +0,0 @@
-const config = {};
-
-if (process.env.NODE_ENV === "development") {
-	config.API_URL = "http://localhost:5000";
-} else {
-	config.API_URL = "https://dashchef.herokuapp.com";
-}
-
-export default config;
--- src/pages/Driver.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Driver = () => {
-	return <div className="bg-gray-100 min-h-screen">Driver sign up form coming soon!</div>;
-};
-
-export default Driver;
